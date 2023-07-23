7b99ad0e8d4b0c0ce478677849b22bb4cd61d012
vinaysalve123 <vinaysalve123@gmail.com>
2023-07-23 14:37:18 +0530

    My First React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [data, setData] = useState(0);
+
+  function getQuote(){
+    try{
+    fetch('https://api.quotable.io/random').then(
+     response => response.json()).then(
+      
+      (quote)=>{
+        setData(quote);
+      }
+     )
+    }
+    catch(e){
+      console.log(e);
+    }
+  }  
+ 
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='Title'>Random Quote Generator</h1>
+        <h3 className='Quote'>Quote: {data.content}</h3>
+        <h3 className='Author'>Author: {data.author}</h3>
+        <h3 className='Author'>Tag: {data.tags}</h3>
+        <button className='Generator' onClick={getQuote}>Get Quote</button>
+      
       </header>
+      
     </div>
   );
 }
